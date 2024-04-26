24538522928e833831f51300672409e7fa391a64
mohtar25 <mokhtar2000naj@gmail.com>
2024-04-26 18:55:31 +0200

    working on the deletion function

--- bdp.js
+++ bdp.js
@@ -1,11 +1,55 @@
+const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const trr = Tree(arr);
+
+trr.root = trr.del(6345);
+
+trr.print();
+
 function Tree(array) {
   const root = BulidTree(array);
+
+  const print = (roots = root) => {
+    if (roots === null) {
+      return;
+    }
+    if (roots.left !== null) print(roots.left);
+    console.log(roots.value);
+    if (roots.right !== null) print(roots.right);
+  };
+
+  const MinValue = (head) => {
+    let min = head.value;
+    while (head.left) {
+      min = head.left.value;
+      head = head.left;
+    }
+    return min;
+  };
+  const del = (roots, value) => {
+    if (roots === null) return roots;
+
+    if (roots.value > value) {
+      roots.right = del(roots.right, value);
+    } else if (roots.value < value) {
+      roots.left = del(roots.left, value);
+    } else {
+      if (roots.left === null) return roots.right;
+      else if (roots.right === null) return roots.left;
+
+      roots.value = MinValue(roots.right);
+      roots.right = del(roots.right, roots.value);
+      return roots;
+    }
+  };
+
+  return { root, print, del };
 }
 
 function MakeNode(data) {
   const value = data;
-  const left = null;
-  const right = null;
+  let left = null;
+  let right = null;
   return { value, left, right };
 }
 
